ffd1dfaedd3d5d881628fb550c63ef8faa44c140
AxonBots <148529190+AxonBots@users.noreply.github.com>
2023-10-20 16:09:07 +0530

    commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -250,7 +250,7 @@ async def start(client, message):
             g = await get_shortlink(chat_id, f"https://telegram.me/{temp.U_NAME}?start=allfiles_{file_id}", True)
         else:
             g = await get_shortlink(chat_id, f"https://telegram.me/{temp.U_NAME}?start=allfiles_{file_id}", False)
-        k = await client.send_message(chat_id=message.from_user.id,text=f"<b>Get All Files in a Single Click!!!\n\n📂 ʟɪɴᴋ ➠ : {g}\n\n<i>Note: This message is deleted in 5 mins to avoid copyrights. Save the link to Somewhere else</i></b>", reply_markup=InlineKeyboardMarkup(
+        k = await client.send_message(chat_id=message.from_user.id,text=f"<b>Get All Files in a Single Click!!!\n\n📂 ʟɪɴᴋ ➠ : {g}\n\n<i>Note: ⚠️ ᴀɢᴀʀ ᴀᴀᴘ ᴍᴏᴠɪᴇ ᴋᴏ ɪꜱ ɢʀᴏᴜᴘ ᴍᴇ ▶️ @Movvies_Request ᴅᴇᴍᴀɴᴅ ᴋᴀʀᴛᴇ ʜᴀɴ ᴛᴏ ᴀᴀᴘᴋᴏ ʟɪɴᴋ ɴᴀʜɪ ᴍɪʟᴇɢᴀ ᴅɪʀᴇᴄᴛ ᴍᴏᴠɪᴇ</i></b>", reply_markup=InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton('📁 ᴍᴏᴠɪᴇ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ 📁', url=g)
