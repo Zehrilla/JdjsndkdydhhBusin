025d1ec8ade786979578d85908e97c9ea0328b95
AxonBots <148529190+AxonBots@users.noreply.github.com>
2023-10-30 11:36:29 +0530

    info.py

--- info.py
+++ info.py
@@ -49,10 +49,10 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'adrinolinks.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '1b0c7308db6c539e9676e900f00f22f93b0b6c45')
-SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'adrinolinks.in')
-SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', '1b0c7308db6c539e9676e900f00f22f93b0b6c45')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'zxlink.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '9a59032a5e7c5ebdd7394d3aa50073596430c9ff')
+SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'zxlink.in')
+SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', '9a59032a5e7c5ebdd7394d3aa50073596430c9ff')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
